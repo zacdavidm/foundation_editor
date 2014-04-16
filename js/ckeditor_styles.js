d2908c6691faac586fe4cdba18a373abea219959
zacdavidm <zacdavidm@gmail.com>
2014-04-16 16:07:55 -0400

    init git with base code from current features module

---
+++ js/ckeditor_styles.js
@@ -0,0 +1,117 @@
+CKEDITOR.stylesSet.add('foundation_editor_styleset', [
+  {
+    name:'Teaser Break',
+    element:'p',
+    attributes:{class:'teaser-break'}
+  },
+  {
+    name:'Lead Paragraph',
+    element:'p',
+    attributes:{class:'lead'}
+  },
+  {
+    name:'Flex Video',
+    element:'div',
+    attributes:{class:'flex-video widescreen'}
+  },
+  {
+    name:'Panel',
+    element:'div',
+    attributes: {class:'panel'}
+  },
+  {
+    name:'Callout Panel',
+    element:'div',
+    attributes: {class:'panel callout'}
+  },
+  {
+    name:'Row',
+    element:'div',
+    attributes: {class:'row'}
+  },
+  {
+    name:'Medium-1 Column',
+    element:'div',
+    attributes: {class:'medium-1 column'}
+  },
+  {
+    name:'Medium-2 Column',
+    element:'div',
+    attributes: {class:'medium-2 column'}
+  },
+  {
+    name:'Medium-3 Column',
+    element:'div',
+    attributes: {class:'medium-3 column'}
+  },
+  {
+    name:'Medium-4 Column',
+    element:'div',
+    attributes: {class:'medium-4 column'}
+  },
+  {
+    name:'Medium-5 Column',
+    element:'div',
+    attributes: {class:'medium-5 column'}
+  },
+  {
+    name:'Medium-6 Column',
+    element:'div',
+    attributes: {class:'medium-6 column'}
+  },
+  {
+    name:'Medium-7 Column',
+    element:'div',
+    attributes: {class:'medium-7 column'}
+  },
+  {
+    name:'Medium-8 Column',
+    element:'div',
+    attributes: {class:'medium-8 column'}
+  },
+  {
+    name:'Medium-9 Column',
+    element:'div',
+    attributes: {class:'medium-9 column'}
+  },
+  {
+    name:'Medium-10 Column',
+    element:'div',
+    attributes: {class:'medium-10 column'}
+  },
+  {
+    name:'Medium-11 Column',
+    element:'div',
+    attributes: {class:'medium-11 column'}
+  },
+  {
+    name:'Medium-12 Column',
+    element:'div',
+    attributes: {class:'medium-12 column'}
+  },
+  { 
+    name: 'Button',	
+    element: 'a',
+    attributes: {class: 'button'}
+  },
+  { 
+    name: 'Tiny Button',	
+    element: 'a',
+    attributes: {class: 'button tiny'}
+  },
+  { 
+    name: 'Small Button',	
+    element: 'a',
+    attributes: {class: 'button small'}
+  },
+  { 
+    name: 'Large Button',	
+    element: 'a',
+    attributes: {class: 'button large'}
+  },
+  { 
+    name: 'Expand Button',	
+    element: 'a',
+    attributes: {class: 'button expand'}
+  }
+]);
